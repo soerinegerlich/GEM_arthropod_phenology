9e2cde50584aaa883cb36d37b4e912c471a87bb8
soerinegerlich <soerinegerlich@hotmail.com>
2021-10-15 10:21:17 +0200

    Snow cover script and climate change data example. Also linear regression model figures

---
+++ Zackenberg_Climate_Snowcover.R
@@ -0,0 +1,88 @@
+.libPaths() #Tjek hvilken library path der bruges naar pakker installeres
+lib = "C:/Users/au511627/Documents/R/win-library/4.1"
+.libPaths(lib)  #kald denne i starten af din session.
+#Der kan opstaa problemer med installering af pakker, hvis pathway til installation er forkert (f.eks. er common) 
+
+#rm(list=ls())
+#Removes all objects from the workspace.
+library(tidyverse)
+library(readxl) 
+library(lubridate) #Functions to work with date-time data.
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+#install.packages("hms")
+library(hms)#Makes it easier to store and format time-of-day values based on the difftime class.
+#install.packages("strucchange")
+library(strucchange) #Testing, monitoring and dating structural changes in linear regression models.
+#install.packages("gvlma")
+library(gvlma) #For assessing linear model assumptions. 
+#install.packages("reshape2")
+library(reshape2) #Outdated, better to use Tidyr. Makes it easier to transform data between wide and long formats.
+#install.packages("data.table")
+library(data.table)
+
+dfsnow <- read.csv("Data/Climate_data_Zackenberg/Snowcover/View_BioBasis_Zackenberg_Data_Abiotics_Snow_and_ice_cover131020211111435653.csv", sep="\t",stringsAsFactors = FALSE, header = TRUE)
+
+dfsnow = dfsnow %>% rename("Date"="ï..Date")
+
+#Add columns: DOY, MONTH, Year
+dfsnow$DOY <- yday(ymd(dfsnow$Date))
+dfsnow$Month <- month(ymd(dfsnow$Date))
+dfsnow$Year <- year(ymd(dfsnow$Date))
+
+dfsnow$SnowCoverFraction[dfsnow$SnowCoverFraction == -9999] <-NA
+dfsnow$SnowCoverFraction <-as.numeric(dfsnow$SnowCoverFraction)
+
+#Identify plots in dataset and remove all irrelevant plots
+sort(unique(dfsnow$Plot))
+
+
+dfsnow<-subset(dfsnow,Plot!="Cas1")
+dfsnow<-subset(dfsnow,Plot!="Cas2")
+dfsnow<-subset(dfsnow,Plot!="Cas3")
+dfsnow<-subset(dfsnow,Plot!="Cas4")
+dfsnow<-subset(dfsnow,Plot!="Cas5")
+dfsnow<-subset(dfsnow,Plot!="Cas6")
+dfsnow<-subset(dfsnow,Plot!="Dry1")
+dfsnow<-subset(dfsnow,Plot!="Dry2Sal7")
+dfsnow<-subset(dfsnow,Plot!="Dry3")
+dfsnow<-subset(dfsnow,Plot!="Dry4")
+dfsnow<-subset(dfsnow,Plot!="Dry5")
+dfsnow<-subset(dfsnow,Plot!="Dry6Pap4")
+dfsnow<-subset(dfsnow,Plot!="Dry7")
+dfsnow<-subset(dfsnow,Plot!="Dry8")
+dfsnow<-subset(dfsnow,Plot!="Eri1")
+dfsnow<-subset(dfsnow,Plot!="Eri2")
+dfsnow<-subset(dfsnow,Plot!="Eri3")
+dfsnow<-subset(dfsnow,Plot!="Eri4")
+dfsnow<-subset(dfsnow,Plot!="Pap1")
+dfsnow<-subset(dfsnow,Plot!="Pap2Sal5")
+dfsnow<-subset(dfsnow,Plot!="Pap3")
+dfsnow<-subset(dfsnow,Plot!="Sal1")
+dfsnow<-subset(dfsnow,Plot!="Sal2")
+dfsnow<-subset(dfsnow,Plot!="Sal3")
+dfsnow<-subset(dfsnow,Plot!="Sal4")
+dfsnow<-subset(dfsnow,Plot!="Sal6")
+dfsnow<-subset(dfsnow,Plot!="Sal7")
+dfsnow<-subset(dfsnow,Plot!="Sax1Si1")
+dfsnow<-subset(dfsnow,Plot!="Sax1Si2")
+dfsnow<-subset(dfsnow,Plot!="Sax1Sil1")
+dfsnow<-subset(dfsnow,Plot!="Sax2Si2")
+dfsnow<-subset(dfsnow,Plot!="Sax2Sil2")
+dfsnow<-subset(dfsnow,Plot!="Sax3Si3")
+dfsnow<-subset(dfsnow,Plot!="Sax3Sil3")
+dfsnow<-subset(dfsnow,Plot!="Si4")
+dfsnow<-subset(dfsnow,Plot!="Sil4")
+dfsnow<-subset(dfsnow,Plot!="Veg1")
+
+dfsnow %>%
+  select(-c(Field_remarks,General_remarks)) -> dfsnow
+
+
+
+
+
+
+               
+
